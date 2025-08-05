aa5a2b73771aaca900967aa59779687917f2e05a
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-08-05 15:58:18 +0330

    created blog app and work it

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,7 +23,7 @@ from website.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    # path('Blog', include('Blog.urls'))
+    path('blog', include('blog.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
